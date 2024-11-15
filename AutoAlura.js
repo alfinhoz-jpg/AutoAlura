761942ff28ce53d30273267be3c9d788cbae8e3b
Alfinhozn <109887797+alfinhoz-jpg@users.noreply.github.com>
2024-11-15 20:25:19 -0300

    BotAlura.js
    Não faça mais de 4 cursos de uma vez!

--- AutoAlura.js
+++ AutoAlura.js
@@ -1,85 +1,25 @@
 // ==UserScript==
 // @name         AutoAlura
 // @namespace    http://tampermonkey.net/
-// @version      1.6
-// @description  Automatiza o Alura. Clique (´) para esconder ou mostrar a Overlay
+// @version      0
+// @description  Ninguem aguenta mais o alura :smile:
 // @author       Alfinhoz
 // @match        https://cursos.alura.com.br/*
 // @license GNU GPLv3
+// @icon https://i.imgur.com/gP1LZq9.png
+// @downloadURL https://update.greasyfork.org/scripts/510828/AutoAlura.user.js
+// @updateURL https://update.greasyfork.org/scripts/510828/AutoAlura.meta.js
 // ==/UserScript==
 
 (function () {
   "use strict";
 
   let isScriptActive = true;
-  let overlayVisible = true;
   let blockClickDelay = 1000;
 
-  function createOverlay() {
-    if (document.getElementById("script-overlay")) return;
-
-    let overlay = document.createElement("div");
-    overlay.id = "script-overlay";
-    overlay.style.position = "fixed";
-    overlay.style.top = "10px";
-    overlay.style.right = "10px";
-    overlay.style.width = "240px";
-    overlay.style.padding = "20px";
-    overlay.style.backgroundColor = "rgba(0, 0, 0, 0.85)";
-    overlay.style.color = "white";
-    overlay.style.fontFamily = "Arial, sans-serif";
-    overlay.style.borderRadius = "8px";
-    overlay.style.zIndex = "9999999";
-    overlay.style.boxShadow = "0 0 20px rgba(0, 0, 0, 0.9)";
-    overlay.style.display = "block";
-    overlay.style.pointerEvents = "auto";
-    overlay.style.overflow = "hidden";
-
-    let title = document.createElement("h3");
-    title.innerText = "Controle do Script";
-    title.style.margin = "0 0 15px";
-    title.style.fontSize = "18px";
-    title.style.color = "#fff";
-    overlay.appendChild(title);
-
-    let toggleButton = document.createElement("button");
-    toggleButton.innerText = "Desativar Script";
-    toggleButton.style.padding = "10px";
-    toggleButton.style.width = "100%";
-    toggleButton.style.marginBottom = "10px";
-    toggleButton.style.cursor = "pointer";
-    toggleButton.style.backgroundColor = "#e74c3c";
-    toggleButton.style.border = "none";
-    toggleButton.style.color = "white";
-    toggleButton.style.borderRadius = "5px";
-    toggleButton.style.fontSize = "14px";
-    toggleButton.onclick = function () {
-      isScriptActive = !isScriptActive;
-      toggleButton.innerText = isScriptActive
-        ? "Desativar Script"
-        : "Ativar Script";
-      toggleButton.style.backgroundColor = isScriptActive
-        ? "#e74c3c"
-        : "#2ecc71";
-    };
-    overlay.appendChild(toggleButton);
-
-    let hideButton = document.createElement("button");
-    hideButton.innerText = "Esconder Overlay";
-    hideButton.style.padding = "10px";
-    hideButton.style.width = "100%";
-    hideButton.style.cursor = "pointer";
-    hideButton.style.backgroundColor = "#3498db";
-    hideButton.style.border = "none";
-    hideButton.style.color = "white";
-    hideButton.style.borderRadius = "5px";
-    hideButton.style.fontSize = "14px";
-    hideButton.onclick = function () {
-      overlay.style.display = "none";
-    };
-    overlay.appendChild(hideButton);
-
-    document.body.appendChild(overlay);
+  const water_mark = document.querySelector(".formattedText");
+    if (water_mark) {
+        water_mark.innerHTML = "É o Alfinhoz vida!";
   }
 
   async function autoPlayVideo() {
@@ -88,9 +28,9 @@
       video.play();
       console.log("Vídeo iniciado.");
       await new Promise((resolve) => setTimeout(resolve, 2000));
-      return true; // Verdadeiro se houver vídeo
+      return true; 
     }
-    return false; // Falso se não houver vídeo
+    return false; 
   }
 
   function clickCorrectAlternatives() {
@@ -109,139 +49,98 @@
             cancelable: true,
           });
           radioInput.dispatchEvent(event);
-          li.classList.add("clicked"); // Adiciona a classe 'clicked' para marcar como clicado
+          li.classList.add("clicked");
         }
       });
-      return true; // Retorna verdadeiro se alternativas foram clicadas
+      return true; 
     }
-    return false; // Retorna falso se não houver alternativas corretas
+    return false; 
+  }
+
+  async function clickMultitaskAlternatives() {
+    const multitaskAlternatives = document.querySelectorAll(
+      'ul.alternativeList li[data-correct="true"]'
+    );
+
+    let clicked = false;
+
+    multitaskAlternatives.forEach((li) => {
+      const checkboxInput = li.querySelector('input[type="checkbox"]');
+      if (checkboxInput && !li.classList.contains("clicked")) {
+        checkboxInput.click();
+        console.log("Clicando na alternativa multitask correta: " + li.innerText);
+        li.classList.add("clicked");
+        clicked = true;
+      }
+    });
+
+    return clicked;
   }
 
   function decodeBase64(encoded) {
     const decoded = atob(encoded);
     return decoded;
   }
+
   async function clickBlocksInOrder() {
     console.log("Iniciando a função clickBlocksInOrder.");
-    const blocksContainer = document.querySelector('.blocks');
+    const blocksContainer = document.querySelector(".blocks");
     if (blocksContainer) {
-        const correctOrder = blocksContainer.dataset.correctOrder;
-        const firstDecoding = decodeBase64(correctOrder);
-        const secondDecoding = decodeBase64(firstDecoding);
-        let decodedTexts = decodeURIComponent(escape(secondDecoding)).split(',').map(text => text.trim());
-        console.log("Textos decodificados:", decodedTexts);
-        let blockButtons = Array.from(blocksContainer.querySelectorAll('.block')); // Alterado para let
-        console.log("Total de botões disponíveis:", blockButtons.length);
+      const correctOrder = blocksContainer.dataset.correctOrder;
+      const firstDecoding = decodeBase64(correctOrder);
+      const secondDecoding = decodeBase64(firstDecoding);
+      let decodedTexts = decodeURIComponent(escape(secondDecoding))
+        .split(",")
+        .map((text) => text.trim());
+      console.log("Textos decodificados:", decodedTexts);
+      let blockButtons = Array.from(blocksContainer.querySelectorAll(".block")); // Alterado para let
+      console.log("Total de botões disponíveis:", blockButtons.length);
 
-        for (const text of decodedTexts) {
-            let blockButton; // Alterado para let
-            let attempts = 0;
+      for (const text of decodedTexts) {
+        let blockButton;
+        let attempts = 0;
 
-            while (attempts < 5) {
-                blockButton = blockButtons.find(button => button.textContent.trim() === text);
-                if (blockButton && !blockButton.classList.contains('clicked')) {
-                    try {
-                        blockButton.click();
-                        console.log("Clicando no bloco: " + text);
-                        blockButton.classList.add('clicked');
-                        await new Promise(resolve => setTimeout(resolve, blockClickDelay));
-                        break; // Sai do loop após clicar
-                    } catch (error) {
-                        console.error("Erro ao clicar no bloco: ", error);
-                    }
-                } else {
-                    console.log("Tentativa " + (attempts + 1) + ": Botão não encontrado ou já clicado para o texto: " + text);
-                }
-                attempts++;
-                await new Promise(resolve => setTimeout(resolve, 500)); // Esperar um pouco antes de tentar novamente
-            }
-
-            // Atualizar a lista de botões
-            blockButtons = blockButtons.filter(button => !button.classList.contains('clicked'));
-        }
-
-        // Após clicar todos os blocos, submeter a resposta
-        await submitAnswer();
-
-        // Esperar 3 segundos antes de clicar em Próxima Atividade
-        await new Promise(resolve => setTimeout(resolve, 3000));
-
-        // Avançar para a próxima atividade
-        let nextButton = document.querySelector(
-            'button.next, ' +
-            'a.next, ' +
-            'input[type="submit"].next, ' +
-            '.task-actions-button .task-body-actions-button .task-actions-button-next, ' +
-            '.bootcamp-next-button, ' +
-            '.bootcamp-primary-button-theme'
-        );
-        if (nextButton) {
-            nextButton.click();
-            console.log("Avançando para a próxima página após clicar nos blocos.");
-        }
-    } else {
-        console.log("Container de blocos não encontrado.");
-    }
-}
-
-
-  function submitAnswer() {
-    let submitButton = document.querySelector("#submitBlocks");
-    if (submitButton) {
-      submitButton.click();
-      console.log("Clicando em 'Submeter resposta'.");
-    }
-  }
-
-  function monitorPage() {
-    let interval = setInterval(async () => {
-      if (!isScriptActive) return; // Se o script estiver desativado, não faz nada
-
-      const videoPlayed = await autoPlayVideo();
-      if (videoPlayed) {
-        console.log("Avançando para a próxima página após o vídeo.");
-        let nextButton = document.querySelector(
-          "button.next, " +
-            "a.next, " +
-            'input[type="submit"].next, ' +
-            ".task-actions-button .task-body-actions-button .task-actions-button-next, " +
-            ".bootcamp-next-button, " +
-            ".bootcamp-primary-button-theme"
-        );
-        if (nextButton) {
-          nextButton.click();
-          return; // Sai da função
-        }
-      }
-
-      const alternativesClicked = clickCorrectAlternatives();
-      if (alternativesClicked) {
-        // Verifica se ainda há alternativas disponíveis
-        let remainingAlternatives = document.querySelectorAll(
-          'ul.alternativeList li[data-correct="true"]:not(.clicked)'
-        );
-        if (remainingAlternatives.length === 0) {
-          let nextButton = document.querySelector(
-            "button.next, " +
-              "a.next, " +
-              'input[type="submit"].next, ' +
-              ".task-actions-button .task-body-actions-button .task-actions-button-next, " +
-              ".bootcamp-next-button, " +
-              ".bootcamp-primary-button-theme"
+        while (attempts < 5) {
+          blockButton = blockButtons.find(
+            (button) => button.textContent.trim() === text
           );
-          if (nextButton) {
-            nextButton.click();
+          if (blockButton && !blockButton.classList.contains("clicked")) {
+            try {
+              blockButton.click();
+              console.log("Clicando no bloco: " + text);
+              blockButton.classList.add("clicked");
+              await new Promise((resolve) =>
+                setTimeout(resolve, blockClickDelay)
+              );
+              break;
+            } catch (error) {
+              console.error("Erro ao clicar no bloco: ", error);
+            }
+          } else {
             console.log(
-              "Avançando para a próxima página após clicar na alternativa correta."
+              "Tentativa " +
+                (attempts + 1) +
+                ": Botão não encontrado ou já clicado para o texto: " +
+                text
             );
           }
+          attempts++;
+          await new Promise((resolve) => setTimeout(resolve, 500));
         }
-        return; // Sai da função
+
+        // Atualizar a lista de botões
+        blockButtons = blockButtons.filter(
+          (button) => !button.classList.contains("clicked")
+        );
       }
 
-      await clickBlocksInOrder(); // Tenta clicar nos blocos
+      // Organizou os blocos, clicar para avançar
+      await submitAnswer();
 
-      // Se não houver blocos, tenta avançar para a próxima atividade
+      // 3 Segundos para a outra atividade
+      await new Promise((resolve) => setTimeout(resolve, 3000));
+
+      // Avançar para a próxima atividade
       let nextButton = document.querySelector(
         "button.next, " +
           "a.next, " +
@@ -252,30 +151,104 @@
       );
       if (nextButton) {
         nextButton.click();
-        console.log("Avançando para a próxima página.");
+        console.log("Avançando para a próxima página após clicar nos blocos.");
       }
-    }, 3000); // Executa a cada 3 segundos
+    } else {
+      console.log("Container de blocos não encontrado.");
+    }
   }
 
-  function toggleOverlay() {
-    let overlay = document.getElementById("script-overlay");
-    if (overlay) {
-      overlay.style.display = overlayVisible ? "none" : "block";
-      overlayVisible = !overlayVisible;
+  function submitAnswer() {
+    let submitButton = document.querySelector("#submitBlocks");
+    if (submitButton) {
+      submitButton.click();
+      console.log("Clicando em 'Submeter resposta'.");
     }
+  }
+
+  async function handlePageActivity() {
+    console.log("Verificando atividade na página.");
+
+    const videoPlayed = await autoPlayVideo();
+    if (videoPlayed) {
+      console.log("Avançando para a próxima página após o vídeo.");
+      let nextButton = document.querySelector(
+        "button.next, " +
+          "a.next, " +
+          'input[type="submit"].next, ' +
+          ".task-actions-button .task-body-actions-button .task-actions-button-next, " +
+          ".bootcamp-next-button, " +
+          ".bootcamp-primary-button-theme"
+      );
+      if (nextButton) {
+        nextButton.click();
+        return;
+      }
+    }
+
+    // Verificando uma escolha
+    const alternativesClicked = clickCorrectAlternatives();
+    if (alternativesClicked) {
+      let nextButton = document.querySelector(
+        "button.next, " +
+          "a.next, " +
+          'input[type="submit"].next, ' +
+          ".task-actions-button .task-body-actions-button .task-actions-button-next, " +
+          ".bootcamp-next-button, " +
+          ".bootcamp-primary-button-theme"
+      );
+      if (nextButton) {
+        nextButton.click();
+        console.log("Avançando para a próxima página após clicar na alternativa correta.");
+      }
+      return;
+    }
+
+    // Verificando multitask
+    const multitaskClicked = await clickMultitaskAlternatives();
+    if (multitaskClicked) {
+      let nextButton = document.querySelector(
+        "button.next, " +
+          "a.next, " +
+          'input[type="submit"].next, ' +
+          ".task-actions-button .task-body-actions-button .task-actions-button-next, " +
+          ".bootcamp-next-button, " +
+          ".bootcamp-primary-button-theme"
+      );
+      if (nextButton) {
+        nextButton.click();
+        console.log("Avançando para a próxima página após clicar na alternativa multitask.");
+      }
+      return;
+    }
+    
+    await clickBlocksInOrder(); 
+    
+    let nextButton = document.querySelector(
+      "button.next, " +
+        "a.next, " +
+        'input[type="submit"].next, ' +
+        ".task-actions-button .task-body-actions-button .task-actions-button-next, " +
+        ".bootcamp-next-button, " +
+        ".bootcamp-primary-button-theme"
+    );
+    if (nextButton) {
+      nextButton.click();
+      console.log("Avançando para a próxima página.");
+    }
+  }
+
+  function monitorPage() {
+    let interval = setInterval(async () => {
+      if (!isScriptActive) return; 
+
+      await handlePageActivity();
+    }, 3000);
   }
 
   function init() {
-    createOverlay();
     monitorPage();
   }
-
-  window.addEventListener("keydown", function (event) {
-    if ((event.key === "Dead" / event.key) === "´") {
-      // Tecla "´"
-      toggleOverlay();
-    }
-  });
 
   init();
 })();
